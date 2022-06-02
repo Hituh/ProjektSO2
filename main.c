febdab1fb2d855954089e8f25b7ab9bb54d456f4
Hituh <okazysssie@gmail.com>
2022-06-02 16:38:20 +0200

    Start of debug + finished Manual

--- main.c
+++ main.c
@@ -12,8 +12,7 @@
 
 int main(int argc, char* argv[])
 {
-    
-    /*------------------------------------Required inputs check------------------------------------*/
+    /*Required inputs check*/
     //if first argument is -h or -help display manual
     if(argc >= 2)
         if(strcmp(argv[1],"-h") == 0 || strcmp(argv[1], "-help") == 0)
@@ -21,15 +20,23 @@ int main(int argc, char* argv[])
             instruction_manual();
             exit(0);
         }
-    int choice;
     int N=10; //Amount of cars
-    int type=0; //0 for Mutex/Semaphore, 1 for Condition Variable
-    while((choice = getopt(argc, argv, "N:T:")) != -1)
+    int type=0; //0 for Mutex/Semaphore, 1 for Conditional Variables
+    int debugmode = 0; //Test for debug mode, 0 for no debug mode, 1 for debug mode
+    int choice;
+    while((choice = getopt(argc, argv, "N:T:Dh:")) != -1)
     {
         switch(choice)
         {
             case 'N':
                 N = atoi(optarg);
+                if(N == 0)
+                {
+                    printf("Invalid argument for N\n");
+                    printf("Valid argument is only a positive integer\n");
+                    printf("Unless you wrote 0, in which case it will be set to default = 10, since there is no point of running the program with 0 cars.\n");
+                    exit(1);
+                }
                 break;
             case 'T':
                 type = atoi(optarg);
@@ -39,12 +46,27 @@ int main(int argc, char* argv[])
                     exit(1);
                 }
                 break;
+            case 'D':
+                debugmode = 1;
+                break;
             default:
                 printf("Wrong arguments\n");
                 exit(1);
         }
     }
-    printf("N: %d\n", N);
-    printf("type: %d\n", type);
-    /*------------------------------------Required inputs check------------------------------------*/
+    /*Required inputs check/
+    /*Debug test*/
+    if(debugmode == 1)
+    {
+        printf("Debug mode enabled\n");
+        printf("N: %d\n", N);
+        printf("type: %d\n", type);
+        exit(0);
+    }
+    /*Debug test*/
+    /*Main Code*/
+
+    //if(type == 0)
+    //if(type == 1)
+    /*Main Code*/
 }
